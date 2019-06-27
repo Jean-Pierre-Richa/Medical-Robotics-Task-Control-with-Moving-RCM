5b547142d52041f74df713ee7f11fc4fd62bd215
Jean Pierre Richa <richajeanpierre@gmail.com>
2019-06-27 23:52:13 +0200

    added struct to hold the robot kinematic values

--- include/kukaDynRCM.h
+++ include/kukaDynRCM.h
@@ -13,7 +13,6 @@
  *       I N C L U D E - F I L E S
  ******************************************************************************/
 
-#include <vector>
 #include <Eigen/Dense>
 
 typedef Eigen::Vector3d vec3d;
@@ -25,7 +24,15 @@ typedef Eigen::MatrixXd MatXd;
  ******************************************************************************/
 
 namespace DYN{
-
+/*
+ * \Struct holding the kinematic parameters
+ */
+struct Params{
+  int l0, l1, l2, l3, l4, l5, l6, l7, off, Xtr, Ytr, Ztr, K;
+  vecxd target_pos;
+  vecxd q;
+  Params() : l0(0), l1(0), l2(0), l3(0), l4(0), l5(0), l6(0), l7(0), off(0), Xtr(0), Ytr(0), Ztr(0), K(0), q(8), target_pos(3){}
+};
 /**
  * \class kukaDyn
  *
@@ -36,7 +43,7 @@ namespace DYN{
 
 class RCM{
   public:
-    static void kukaDynRCM(vecxd& q);
+    static void kukaDynRCM(Params& kinParams);
 
   }; // class kukaDyn
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,28 @@
 
 using namespace std;
 
+DYN::Params kinParams;
+
 int main(int argc, char** argv){
   (void)argc;
   (void)argv;
 
-  vecxd q(8);
-  q << 1, 2, 3, 4, 5, 6, 7, 8;
+  kinParams.l0=1;
+  kinParams.l1=2;
+  kinParams.l2=3;
+  kinParams.l3=4;
+  kinParams.l4=5;
+  kinParams.l5=6;
+  kinParams.l6=7;
+  kinParams.l7=8;
+  kinParams.off=10;
+  kinParams.Xtr = 3;
+  kinParams.Ytr = 6;
+  kinParams.Ztr = 5;
+  kinParams.target_pos << 1, 4, 7;
+  kinParams.q << 1, 2, 3, 4, 5, 6, 7, 8;
 
-  DYN::RCM::kukaDynRCM(q);
+  DYN::RCM::kukaDynRCM(kinParams);
+
 
 }
